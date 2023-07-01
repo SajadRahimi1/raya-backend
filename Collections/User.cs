d37dad95133c5daea28f3700f316e50f8047efc6
Sajad <sajadrahimi0101@gmail.com>
2023-07-01 17:15:56 +0330

    make Id nullable

--- Collections/User.cs
+++ Collections/User.cs
@@ -5,7 +5,7 @@ public class User
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [BsonElement("name")]
     public string? Name { get; set; }
