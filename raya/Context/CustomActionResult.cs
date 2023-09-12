fab086d5fac25f16aff2ad7ce872362d3e7d94db
Sajad <sajadrahimi0101@gmail.com>
2023-09-12 16:52:32 +0330

    test some return json

--- raya/Context/CustomActionResult.cs
+++ raya/Context/CustomActionResult.cs
@@ -28,7 +28,7 @@ public class CustomActionResult : IActionResult
 
     public async Task ExecuteResultAsync(ActionContext context)
     {
-        var objectResult = new ObjectResult(_result.ErrorMessage ?? _result.Data)
+        var objectResult = new ObjectResult(_result ?? _result)
         {
             StatusCode = _result.statusCodes,
         };
