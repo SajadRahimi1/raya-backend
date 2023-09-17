d1b28c13b9c45c021cdfa6f17f9311e5bce8cd5e
Sajad <sajadrahimi0101@gmail.com>
2023-09-17 14:19:40 +0330

    add some function for admin repository

--- raya/Interfaces/IAdminRepository.cs
+++ raya/Interfaces/IAdminRepository.cs
@@ -1,4 +1,7 @@
 public interface IAdminRepository
 {
     Task<Admin?> getAdminByToken(string token);
+    Task<CustomActionResult> addAdmin(Admin admin);
+    Task<CustomActionResult> editAdmin(Admin admin);
+    Task<CustomActionResult> getAllAdmin();
 }
--- raya/Repository/AdminRepository.cs
+++ raya/Repository/AdminRepository.cs
@@ -9,9 +9,28 @@ public class AdminRepository : IAdminRepository
         _appDbContext = appDbContext;
     }
 
+    public async Task<CustomActionResult> addAdmin(Admin admin)
+    {
+        var addedAdmin = await _appDbContext.Admins.AddAsync(admin);
+        return new CustomActionResult(new Result { Data = addedAdmin.Entity });
+    }
+
+    public async Task<CustomActionResult> editAdmin(Admin admin)
+    {
+        var editedAdmin = _appDbContext.Admins.Update(admin);
+        await _appDbContext.SaveChangesAsync();
+        return new CustomActionResult(new Result { Data = editedAdmin.Entity });
+
+    }
+
     public async Task<Admin?> getAdminByToken(string token)
     {
         return await _appDbContext.Admins.SingleOrDefaultAsync(_ => _.token == token);
 
+    }
+
+    public async Task<CustomActionResult> getAllAdmin()
+    {
+        return new CustomActionResult(new Result { Data = await _appDbContext.Admins.ToListAsync() });
     }
 }
